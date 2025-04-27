7b1fbc15c2aa8875c7645d43e1c91ae76a54ea59
Maria Wysogląd <mw431552@students.mimuw.edu.pl>
2025-04-27 13:39:12 +0200

    update

--- mw431552/algorithm.cpp
+++ mw431552/algorithm.cpp
@@ -3,15 +3,15 @@
 #include <fstream>
 #include <string>
 
-// std::string prepare_output_path(const std::string& output_file) {
-//     if (output_file.find('/') == std::string::npos && output_file.find('\\') == std::string::npos) {
-//         // No slash -> assume it's a file name, save in current directory
-//         return "./" + output_file;
-//     } else {
-//         // Has slash -> treat as path to file
-//         return output_file;
-//     }
-// }
+std::string another_output_path(const std::string& output_file) {
+    if (output_file.find('/') == std::string::npos && output_file.find('\\') == std::string::npos) {
+        // No slash -> assume it's a file name, save in current directory
+        return "./" + output_file;
+    } else {
+        // Has slash -> treat as path to file
+        return output_file;
+    }
+}
 
 // void worker(const std::vector<std::vector<float>>& graph, int num_iter, double alpha, double beta, double evaporate, int seed, std::string output_file) {
 //     std::cout << "Running WORKER algorithm...\n";
@@ -38,7 +38,7 @@ void queen(const std::vector<std::vector<float>>& graph, int num_iter, double al
     std::cout << "Parameters: NUM_ITER=" << num_iter << ", ALPHA=" << alpha 
               << ", BETA=" << beta << ", EVAPORATE=" << evaporate << ", SEED=" << seed << std::endl;
 
-    std::string full_output_path = prepare_output_path(output_file);
+    std::string full_output_path = another_output_path(output_file);
 
     std::ofstream ofs(full_output_path);
     if (!ofs.is_open()) {
